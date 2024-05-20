40a22683d535a4e26fc66696966805bf993c82f0
josa <josafat30000@gmail.com>
2024-05-20 03:44:23 -0600

    last

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-        external: [
-            "../node-modules/@supabase/supabase-js", // ignore react stuff
-            "../node-modules/xlsx",
-        ],
-    }
-},
+
 })
