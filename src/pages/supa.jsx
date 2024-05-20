e6d3787ff23e9fec5eeee793b139aab991ef638c
josa <josafat30000@gmail.com>
2024-05-20 03:21:04 -0600

    yaaaaaaa

--- src/pages/supa.jsx
+++ src/pages/supa.jsx
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '../node-modules/@supabase/supabase-js';
 const supabaseUrl ="https://udmtmyzexwnrnggeugit.supabase.co";
 
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVkbXRteXpleHducm5nZ2V1Z2l0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI1MzM3NTIsImV4cCI6MjAyODEwOTc1Mn0.UkflMXNLJugRiAAlpTqkV0gt_Ir2BOXKmGKTCtrosYQ";
